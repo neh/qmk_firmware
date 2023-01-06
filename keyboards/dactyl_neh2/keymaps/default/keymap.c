3a6b14f530598f0bc0be0c7df09e7f8b12aec342
Nathan Howell <nath@nhowell.net>
2023-01-06 11:40:53 -0800

    layout updates

--- keyboards/dactyl_neh2/keymaps/default/keymap.c
+++ keyboards/dactyl_neh2/keymaps/default/keymap.c
@@ -17,28 +17,30 @@
 /* #define LN_M LT(4, KC_M) */
 
 #define CTL_ESC CTL_T(KC_ESC)
-#define NMSSPC LT(_NUMSYM, KC_SPC)
-#define NMS_ENT LT(_NUMSYM, KC_ENT)
-#define MOUSTAB LT(_MOUSE, KC_TAB)
-#define MOUSESC LT(_MOUSE, KC_ESC)
-#define NUM_ESC LT(_NUMSYM, KC_ESC)
-#define NAVSPC LT(_NAV, KC_SPC)
+#define NMSSPC LT(_NUMS, KC_SPC)
+#define NMS_ENT LT(_NUMS, KC_ENT)
+#define MOUSTAB LT(_MOUS, KC_TAB)
+#define MOUSESC LT(_MOUS, KC_ESC)
+#define NUM_ESC LT(_NUMS, KC_ESC)
+#define NAVSPC LT(_NAVI, KC_SPC)
 
 #define OK_LSFT OSM(MOD_LSFT)
 #define OK_LALT OSM(MOD_LALT)
 #define OK_LCTL OSM(MOD_LCTL)
 #define OK_LGUI OSM(MOD_LGUI)
 
-#define OK_MOUS OSL(_MOUSE)
+#define OK_MOUS OSL(_MOUS)
+#define OK_FUNC OSL(_FUNC)
 
 #define OK_GUST OSM(MOD_LGUI | MOD_LSFT)
 #define OK_GUCT OSM(MOD_LGUI | MOD_LCTL)
 
 
 #define _BASE 0
-#define _NAV 1
-#define _NUMSYM 2
-#define _MOUSE 3
+#define _NAVI 1
+#define _NUMS 2
+#define _MOUS 3
+#define _FUNC 4
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
@@ -47,30 +49,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           DV_A,    DV_O,    DV_E,    DV_U,    DV_I,    DV_D,    DV_H,    DV_T,    DV_N,    DV_S,
           DV_SCLN, DV_Q,    DV_J,    DV_K,    DV_X,    DV_B,    DV_M,    DV_W,    DV_V,    DV_Z,
                    _______, _______,                                     _______, _______,
-                            OK_LGUI, OSL(_NAV),                     KC_SPC, OSL(_NUMSYM),
-                          /* OSL(_NAV), KC_SPC,                     OSL(_NUMSYM), OK_LGUI, */
-                                     OK_LCTL, OK_MOUS,  _______, KC_ENT,
+                            OK_LGUI, OSL(_NAVI),                     KC_SPC, OSL(_NUMS),
+                          /* OSL(_NAVI), KC_SPC,                     OSL(_NUMS), OK_LGUI, */
+                                     OK_LCTL, OK_MOUS,  OK_FUNC, KC_ENT,
                                      NUM_ESC, KC_ESC,   _______, KC_BSPC
     ),
-    [_NAV] = LAYOUT(
-          QK_BOOT, _______, _______, OK_GUST, _______,  C(DV_R), CS_TAB,  KC_PGDN, KC_PGUP, C_TAB,
-          OK_LGUI, OK_LALT, OK_LCTL, OK_LSFT, KC_VOLU,  CAPSWRD, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-          KC_MPRV, KC_MUTE, KC_MPLY, KC_MNXT, KC_VOLD,  C(DV_L), _______, KC_END,  KC_HOME, _______,
+    [_NAVI] = LAYOUT(
+          QK_BOOT, KC_VOLU, KC_VOLD, OK_GUST, _______,  C(DV_R), CS_TAB,  KC_PGDN, KC_PGUP, C_TAB,
+          OK_LGUI, OK_LALT, OK_LCTL, OK_LSFT, CAPSWRD,  _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+          KC_MPRV, KC_MUTE, KC_MPLY, KC_MNXT, _______,  C(DV_L), _______, KC_END,  KC_HOME, _______,
                    _______, _______,                                      _______, _______,
                             _______, _______,                    _______, _______,
                                      _______, _______,  _______, CAPSWRD,
                                      _______, _______,  _______, KC_DEL
     ),
-    [_NUMSYM] = LAYOUT(
-          DV_LBRC, KC_4,    KC_5,    KC_6,    DV_RBRC,  _______, OK_GUST, OK_GUCT, _______, QK_BOOT,
-          KC_0,    KC_1,    KC_2,    KC_3,    DV_SLSH,  CAPSWRD, OK_LSFT, OK_LCTL, OK_LALT, OK_LGUI,
-          DV_EQL,  KC_7,    KC_8,    KC_9,    DV_GRV,   _______, _______, _______, _______, _______,
+    [_NUMS] = LAYOUT(
+          DV_LBRC, KC_9,    KC_8,    KC_7,    DV_RBRC,  _______, OK_GUST, OK_GUCT, _______, QK_BOOT,
+          KC_0,    KC_3,    KC_2,    KC_1,    DV_SLSH,  CAPSWRD, OK_LSFT, OK_LCTL, OK_LALT, OK_LGUI,
+          DV_EQL,  KC_6,    KC_5,    KC_4,    DV_GRV,   KC_ASTG, _______, _______, _______, _______,
                    _______, _______,                                      _______, _______,
                             DV_BSLS, DV_DOT,                     _______, _______,
                                      DV_MINS, _______,  _______, _______,
                                      _______, _______,  _______, _______
     ),
-    [_MOUSE] = LAYOUT(
+    [_MOUS] = LAYOUT(
           QK_BOOT, _______, _______, _______, _______,  _______, _______, KC_WH_D, KC_WH_U, _______,
           OK_LGUI, OK_LALT, OK_LCTL, OK_LSFT, CAPSWRD,  _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
           _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
@@ -79,10 +81,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                      _______, _______,  _______, KC_BTN3,
                                      _______, _______,  _______, _______
     ),
-    [4] = LAYOUT(
-          KC_F11,  KC_F4,   KC_F5,   KC_F6,   _______,  _______, _______, _______, _______, QK_BOOT,
-          KC_F10,  KC_F1,   KC_F2,   KC_F3,   _______,  CAPSWRD, OK_LSFT, OK_LCTL, OK_LALT, OK_LGUI,
-          KC_F12,  KC_F7,   KC_F8,   KC_F9,   _______,  _______, _______, _______, _______, _______,
+    [_FUNC] = LAYOUT(
+          KC_F12,  KC_F9,   KC_F8,   KC_F7,   _______,  _______, _______, _______, _______, QK_BOOT,
+          KC_F10,  KC_F3,   KC_F2,   KC_F1,   _______,  CAPSWRD, OK_LSFT, OK_LCTL, OK_LALT, OK_LGUI,
+          KC_F11,  KC_F6,   KC_F5,   KC_F4,   _______,  _______, _______, _______, _______, _______,
                    _______, _______,                                      _______, _______,
                             _______, _______,                    _______, _______,
                                      _______, _______,  _______, _______,
@@ -140,6 +142,7 @@ const uint16_t PROGMEM dash_combo[] = {DV_J, DV_K, COMBO_END};
 const uint16_t PROGMEM ctl_x_combo[] = {DV_M, DV_W, COMBO_END};
 const uint16_t PROGMEM ctl_c_combo[] = {DV_G, DV_C, COMBO_END};
 const uint16_t PROGMEM tab_combo[] = {DV_W, DV_V, COMBO_END};
+const uint16_t PROGMEM tab2_combo[] = {KC_HOME, KC_END, COMBO_END};
 /* const uint16_t PROGMEM lctl_combo[] = {DV_O, DV_E, COMBO_END}; */
 /* const uint16_t PROGMEM rctl_combo[] = {DV_T, DV_N, COMBO_END}; */
 combo_t key_combos[COMBO_COUNT] = {
@@ -148,6 +151,7 @@ combo_t key_combos[COMBO_COUNT] = {
     COMBO(ctl_x_combo, LCTL(DV_X)),
     COMBO(ctl_c_combo, LCTL(DV_C)),
     COMBO(tab_combo, KC_TAB),
+    COMBO(tab2_combo, KC_TAB),
     /* COMBO(lctl_combo, OK_LCTL), */
     /* COMBO(rctl_combo, OK_LCTL), */
 };
